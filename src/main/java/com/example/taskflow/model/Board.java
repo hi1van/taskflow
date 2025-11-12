ccdd3602e329c44dede13824d5b6b68be85dc4a0
Ivan Zhuang <ivan.c.zhuang@gmail.com>
2025-11-12 19:23:44 +1100

    created models

--- src/main/java/com/example/taskflow/model/Board.java
+++ src/main/java/com/example/taskflow/model/Board.java
@@ -1,4 +1,20 @@
 package com.example.taskflow.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Board {
+    private String boardId;
+    private String name;
+    private String description;
+    private String ownerId;
+    private List<String> members;
+    private Date createdAt;
 }
--- src/main/java/com/example/taskflow/model/Task.java
+++ src/main/java/com/example/taskflow/model/Task.java
@@ -1,4 +1,21 @@
 package com.example.taskflow.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Task {
+    private String taskId;
+    private String boardId;
+    private String title;
+    private String description;
+    private String status;
+    private String assigneeId;
+    private Date createdAt;
+    private Date updatedAt;
 }
